46d86bc3ac71500cfc785db7579d065dd02d5a5b
Aditya Addepalli <adi.addepalli@gmail.com>
2025-02-14 00:43:18 -0500

    feat: update project to One Piece chat app with favicons

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "One Piece Chat",
+  description: "Chat with the One Piece manga",
+  url: "https://onepiece.agnilabs.xyz",
+  links: {
+    twitter: "https://x.com/agnilabs",
+    github: "https://github.com/agnilabs",
+  },
+};
